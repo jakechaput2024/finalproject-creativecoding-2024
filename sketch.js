51542b44e813194803e23c140ada446849cb033e
JAKE CHAPUT <JWC21@FSU.EDU>
2024-11-17 16:27:18 -0500

    changed background and star line colors, tryna figure out how to add images and music rn

--- sketch.js
+++ sketch.js
@@ -5,6 +5,17 @@ let lineEndingX, lineEndingY;
 let allConnected = false; //when lines connect dots, this'll become true
 let numberofDots;
 let state = 0;
+let img;
+let gradient;
+let text1, text2;
+
+
+function preload() {
+  img = loadImage('/assets/Rat.png');
+  gradient = loadImage('/assets/gradient.png');
+  text1 = loadImage('/assets/text1.gif');
+  text2 = loadImage('/assets/text2.gif');
+}
 
 function setup() {
 
@@ -12,71 +23,42 @@ function setup() {
 
   createCanvas(windowWidth, windowHeight);
   for (let i = 0; i < numberofDots; i++) {
-    dots[i] = new Circle(30);
+    dots[i] = new Circle(25);
   }
+  
 }
 
 function draw() {
-  background(220);
+  //background(2, 13, 23);
+  image(gradient, 0, 0)
 
   switch (state) {
     case 0:
       background("black");
-      textSize(32);
-      fill(255);
-      text("Try to connect all of the dots in order!", 500, 50);
+      
+      image(text1, 400, 500);
 
-      if (millis() > 7000) {
+      if (millis() > 4900) {
         state = 1;
       }
 
       break;
 
-
     case 1:
+      background("black");
+      
+      image(text2, 400, 500);
 
-      // old lines
-      stroke("red");
-      for (let i = 0; i < lines.length; i++) {
-        const l = lines[i];
-        l.draw();
-      }
-
-      lineEndingX = mouseX;
-      lineEndingY = mouseY;
-
-      // the dots
-      for (let i = 0; i < dots.length; i++) {
-        const dot = dots[i]
-        dot.draw();
-
-
-        textSize(16);
-        textAlign(CENTER, CENTER);
-        text(i + 1, dot.x, dot.y - 20);
-
-        if (dist(mouseX, mouseY, dot.x, dot.y) < 7) {
-          lineEndingX = dot.x;
-          lineEndingY = dot.y;
-        }
-      }
-
-      // current line
-      stroke("black");
-      line(mx, my, lineEndingX, lineEndingY);
-
-      if (allConnected) {
-        fill("green");
-        textSize(32);
-        textAlign(CENTER, CENTER);
-        text("You Won!", width / 2, height / 2);
+      if (millis() > 9900) {
         state = 2;
       }
+
       break;
 
     case 2:
+
       // old lines
-      stroke("red");
+      stroke(199, 249, 255);
       for (let i = 0; i < lines.length; i++) {
         const l = lines[i];
         l.draw();
@@ -102,7 +84,7 @@ function draw() {
       }
 
       // current line
-      stroke("black");
+      stroke(199, 249, 255);
       line(mx, my, lineEndingX, lineEndingY);
 
       if (allConnected) {
@@ -110,8 +92,12 @@ function draw() {
         textSize(32);
         textAlign(CENTER, CENTER);
         text("You Won!", width / 2, height / 2);
+        image(img, 0, 0);
       }
       break;
+
+    case 3:
+      
   }
 }
 
