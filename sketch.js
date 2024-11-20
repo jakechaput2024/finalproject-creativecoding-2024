a4b52c067f011efad0cb6de227a0bfe47104f656
JAKE CHAPUT <JWC21@FSU.EDU>
2024-11-20 09:34:23 -0500

    sound plays when you press either lowercase or uppercase M, need to figure out how to make it so that it only happens once

--- sketch.js
+++ sketch.js
@@ -21,11 +21,11 @@ function preload() {
 
 function setup() {
   song.play();
-  console.log
 
   allCoordinates = [
     [createVector(322, 65), createVector(128, 317), createVector(365, 534), createVector(800, 634), createVector(1076, 502), createVector(1186, 357), createVector(1136, 283), createVector(973, 86)],
-    [createVector(258, 179), createVector(409, 385), createVector(1011, 191), createVector(1238, 306), createVector(1217, 452)]
+    [createVector(258, 179), createVector(409, 385), createVector(1011, 191), createVector(1238, 306), createVector(1217, 452)],
+    [createVector(356, 173), createVector(425, 496), createVector(990, 133), createVector(973, 450), createVector(356, 173)]
   ]
 
   createCanvas(1399, 703);
@@ -52,11 +52,11 @@ function draw() {
       break;
 
     case 2:
-      fill(255, 60, 100);
+      fill(199, 249, 255);
       text("(" + mouseX + ", " + mouseY + ")", mouseX, mouseY);
 
       // old lines
-      stroke(199, 249, 255); //need this stroke so the older lines show
+      stroke(199, 249, 255);
       for (let i = 0; i < lines.length; i++) {
         const l = lines[i];
         l.draw();
@@ -82,10 +82,6 @@ function draw() {
       line(mx, my, lineEndingX, lineEndingY);
 
       if (allConnected) {
-        // fill("green");
-        // textSize(32);
-        // textAlign(CENTER, CENTER);
-        // text("You Won!", width / 2, height / 2);
         image(img, 0, 0);
         if (millis() > 30000) { //4900 (work on setting the millis > number)
           state = 3;
@@ -103,49 +99,90 @@ function draw() {
 
     case 3:
 
-    fill(245, 175, 78);
-    stroke(199, 249, 255); //need this stroke so the older lines show
+      fill(199, 249, 255);
+
+      //old lines
+      stroke(199, 249, 255); 
       for (let i = 0; i < lines.length; i++) {
         const l = lines[i];
         l.draw();
       }
 
-    noStroke();
+      noStroke();
 
-    lineEndingX = mouseX;
-    lineEndingY = mouseY;
+      lineEndingX = mouseX;
+      lineEndingY = mouseY;
 
-    for (let i = 0; i < allCoordinates[stage].length; i++) {
-      const xy = allCoordinates[stage][i];
-      circle(xy.x, xy.y, 20); 
-      if (dist(mouseX, mouseY, xy.x, xy.y) < 7) {
-        lineEndingX = xy.x;
-        lineEndingY = xy.y;
-      }     
-    }
-
-    stroke(199, 249, 255);
-    line(mx, my, lineEndingX, lineEndingY);
-
-    if (allConnected) {
-      // fill("green");
-      // textSize(32);
-      // textAlign(CENTER, CENTER);
-      // text("You Won!", width / 2, height / 2);
-      image(img, 0, 0);
-      if (millis() > 30000) { //4900 (work on setting the millis > number)
-        state = 3;
-        stage = (stage + 1) % allCoordinates.length;
-        console.log(stage);
-
-        lines = [];
-        allConnected = false;
-        currentdotIndex = 0;
-        mx = undefined;
-        my = undefined;
+      for (let i = 0; i < allCoordinates[stage].length; i++) {
+        const xy = allCoordinates[stage][i];
+        circle(xy.x, xy.y, 20);
+        if (dist(mouseX, mouseY, xy.x, xy.y) < 7) {
+          lineEndingX = xy.x;
+          lineEndingY = xy.y;
+        }
       }
-    }
-    break;
+
+      stroke(199, 249, 255);
+      line(mx, my, lineEndingX, lineEndingY);
+
+      if (allConnected) {
+        image(img, 0, 0);
+        if (millis() > 30000) { //4900 (work on setting the millis > number)
+          state = 4;
+          stage = (stage + 1) % allCoordinates.length;
+          console.log(stage);
+
+          lines = [];
+          allConnected = false;
+          currentdotIndex = 0;
+          mx = undefined;
+          my = undefined;
+        }
+      }
+      break;
+
+    case 4:
+
+      fill(199, 249, 255);
+
+      //old lines
+      stroke(199, 249, 255);
+      for (let i = 0; i < lines.length; i++) {
+        const l = lines[i];
+        l.draw();
+      }
+
+      noStroke();
+
+      lineEndingX = mouseX;
+      lineEndingY = mouseY;
+
+      for (let i = 0; i < allCoordinates[stage].length; i++) {
+        const xy = allCoordinates[stage][i];
+        circle(xy.x, xy.y, 20);
+        if (dist(mouseX, mouseY, xy.x, xy.y) < 7) {
+          lineEndingX = xy.x;
+          lineEndingY = xy.y;
+        }
+      }
+
+      stroke(199, 249, 255);
+      line(mx, my, lineEndingX, lineEndingY);
+
+      if (allConnected) {
+        image(img, 0, 0);
+        if (millis() > 30000) { //4900 (work on setting the millis > number)
+          state = 4;
+          stage = (stage + 1) % allCoordinates.length;
+          console.log(stage);
+
+          lines = [];
+          allConnected = false;
+          currentdotIndex = 0;
+          mx = undefined;
+          my = undefined;
+        }
+      }
 
   }
 }
@@ -154,18 +191,28 @@ function mouseClicked() {
   for (let i = 0; i < allCoordinates[stage].length; i++) {
     const xy = allCoordinates[stage][i];
     if (dist(mouseX, mouseY, xy.x, xy.y) < 7) {
-      if (i === currentdotIndex) { 
+      if (i === currentdotIndex) {
         currentdotIndex++;
-      let line = new Line(mx, my, xy.x, xy.y);
-      lines.push(line);
-      mx = xy.x;
-      my = xy.y;
+        let line = new Line(mx, my, xy.x, xy.y);
+        lines.push(line);
+        mx = xy.x;
+        my = xy.y;
 
-      if (lines.length === allCoordinates[stage].length) {
-        allConnected = true;
-      }
-    } else {}
+        if (lines.length === allCoordinates[stage].length) {
+          allConnected = true;
+        }
+      } else { }
       break;
     }
+  }
+}
+
+function keyPressed() {
+  if (key === 'm') {
+    song.play();
+    song.loop();
+  } else if (key === 'M') {
+    song.play();
+    song.loop();
   }
 }
